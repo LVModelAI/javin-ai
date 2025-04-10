cc7c5d9401c4f9927599514666544c2fc222c01f
lvmShantanu <shantanu@lvmodel.com>
2025-04-10 17:50:50 +0530

    removed forgot password from register page

--- apps/javin/app/(auth)/register/page.tsx
+++ apps/javin/app/(auth)/register/page.tsx
@@ -88,6 +88,7 @@ export default function Page() {
           action={handleSubmit}
           defaultEmail={email}
           fieldErrors={state.fieldErrors}
+          forgotPasswordNeeded={false}
         >
           <SubmitButton isSuccessful={isSuccessful}>Sign Up</SubmitButton>
           <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
