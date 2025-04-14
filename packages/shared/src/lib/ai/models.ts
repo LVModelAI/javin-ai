0687e9859acdb8d4117c3f9821f2dab05dbcb759
lvmShantanu <shantanu@lvmodel.com>
2025-04-14 13:22:49 +0530

    chat model revert

--- packages/shared/src/lib/ai/models.ts
+++ packages/shared/src/lib/ai/models.ts
@@ -7,12 +7,12 @@ import {
 } from "ai";
 import { google } from "@ai-sdk/google";
 
-export const DEFAULT_CHAT_MODEL: string = "gpt-4o-mini";
+export const DEFAULT_CHAT_MODEL: string = "chat-model-small";
 
 export const myProvider: any = customProvider({
   languageModels: {
-    "gpt-4o-mini": openai("gpt-4o-mini"),
-    "gpt-4o": openai("gpt-4o"),
+    "chat-model-small": openai("gpt-4o-mini"),
+    "chat-model-large": openai("gpt-4o"),
     "chat-model-reasoning": wrapLanguageModel({
       model: fireworks("accounts/fireworks/models/deepseek-r1"),
       middleware: extractReasoningMiddleware({ tagName: "think" }),
@@ -38,12 +38,12 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: "gpt-4o-mini",
+    id: "chat-model-small",
     name: "Gpt 4o mini",
     description: "Small model for fast, lightweight tasks",
   },
   {
-    id: "gpt-4o",
+    id: "chat-model-large",
     name: "Gpt 4o",
     description: "Large model for complex, multi-step tasks",
   },
