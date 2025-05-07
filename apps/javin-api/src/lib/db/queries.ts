f4df699b1e8b27c4324fd6ca0036086220d9d40f
mohammedLVM <mohammad@lvmodel.com>
2025-05-07 16:03:45 +0530

    api based mode selection

--- apps/javin-api/src/lib/db/queries.ts
+++ apps/javin-api/src/lib/db/queries.ts
@@ -1,14 +1,44 @@
-import { message, Message, toolTracking, ToolTracking } from "./schema";
+import {
+  ConsumerEnumType,
+  consumerTable,
+  message,
+  Message,
+  nexusMessage,
+  toolTracking,
+  ToolTracking,
+} from "./schema";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as Sentry from "@sentry/nextjs";
+import { PgTableWithColumns } from "drizzle-orm/pg-core";
+import { eq } from "drizzle-orm";
 
 const client = postgres(process.env.POSTGRES_URL!);
 const db = drizzle(client);
 
-export async function saveMessages({ messages }: { messages: Array<Message> }) {
+// Save messages in the target table.
+// table can be for Sentient or Nexus
+export async function saveMessages({
+  consumerName,
+  messages,
+}: {
+  consumerName: ConsumerEnumType;
+  messages: Array<Message>;
+}) {
   try {
-    return await db.insert(message).values(messages);
+    let targetTable: PgTableWithColumns<any>;
+    switch (consumerName) {
+      case "SENTIENT":
+        targetTable = message;
+        break;
+      case "NEXUS":
+        targetTable = nexusMessage;
+        break;
+      default:
+        targetTable = message;
+        break;
+    }
+    return await db.insert(targetTable).values(messages);
   } catch (error) {
     console.error("Failed to save messages in database", error);
     Sentry.captureException(error);
@@ -23,6 +53,20 @@ export async function saveToolTracking({
 }) {
   try {
     return await db.insert(toolTracking).values(toolTrackingData);
+  } catch (error) {
+    console.error("Failed to save tool tracking data in database", error);
+    Sentry.captureException(error);
+    throw error;
+  }
+}
+
+export async function getConsumerUsingApiKey({ apiKey }: { apiKey: string }) {
+  try {
+    const [consumer] = await db
+      .select()
+      .from(consumerTable)
+      .where(eq(consumerTable.apiKey, apiKey));
+    return consumer;
   } catch (error) {
     console.error("Failed to save tool tracking data in database", error);
     Sentry.captureException(error);
