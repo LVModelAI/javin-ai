7b57327a2d9c5716300b79fd45cb01216fcf091f
mohammedLVM <mohammad@lvmodel.com>
2025-05-12 21:25:57 +0530

    little documentation

--- apps/javin-api/src/lib/db/redis.ts
+++ apps/javin-api/src/lib/db/redis.ts
@@ -5,3 +5,5 @@ export const redis = new Redis({
   token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
 
+// ratelimit:${apiKey} is the key in the redis db with value as the number of requests made by the consumer in the last 60 seconds
+// and ttl is 60 seconds
