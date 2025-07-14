8c21f603d1066ccae30860f11335a50bb99ec3bb
lvmShantanu <shantanu@lvmodel.com>
2025-07-14 13:50:47 +0530

    hash table type export

--- apps/javin-api/src/lib/db/schema.ts
+++ apps/javin-api/src/lib/db/schema.ts
@@ -72,3 +72,5 @@ export const hashTable = pgTable("HashTable", {
   hash: varchar("hash").primaryKey().notNull(),
   transactionId: varchar("transactionId").notNull(),
 });
+
+export type HashTable = InferSelectModel<typeof hashTable>;
