65b98c4127efb5e92aa8695450a764116b4d67b8
mohammedLVM <mohammad@lvmodel.com>
2025-09-29 18:11:24 +0530

    exlucind middleware on health-check url

--- apps/javin/app/api/health-check/route.ts
+++ apps/javin/app/api/health-check/route.ts
@@ -1,25 +1,31 @@
-
 import { NextRequest, NextResponse } from "next/server";
 import { GET as healthCheckGET } from "@javin/shared/health-check/main";
 
 export async function GET(request: NextRequest) {
   // Check for authorization header
+  //   const authHeader = request.headers.get("authorization");
+  //   const apiKey = "hardcoding this for now, no secrecey needed here tbh for health check";
+
+  //   if (!apiKey) {
+  //     return NextResponse.json(
+  //       { error: "Health check API key not configured" },
+  //       { status: 500 }
+  //     );
+  //   }
+
+  //   if (!authHeader || authHeader !== `Bearer ${apiKey}`) {
+  //     return NextResponse.json(
+  //       { error: "Unauthorized" },
+  //       { status: 401 }
+  //     );
+  //   }
+
   const authHeader = request.headers.get("authorization");
-  const apiKey = "hardcoding this for now, no secrecey needed here tbh for health check";
-  
-  if (!apiKey) {
-    return NextResponse.json(
-      { error: "Health check API key not configured" },
-      { status: 500 }
-    );
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
   }
-  
-  if (!authHeader || authHeader !== `Bearer ${apiKey}`) {
-    return NextResponse.json(
-      { error: "Unauthorized" },
-      { status: 401 }
-    );
-  }
-  
+
   return healthCheckGET();
 }
