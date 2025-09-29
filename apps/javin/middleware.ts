daf70344a96d1d56bbb75dc1d3a526d35d511b21
mohammedLVM <mohammad@lvmodel.com>
2025-09-29 18:19:01 +0530

    exlucind middleware on health-check url

--- apps/javin/middleware.ts
+++ apps/javin/middleware.ts
@@ -1,17 +1,7 @@
 import NextAuth from "next-auth";
-
 import { authConfig } from "@/app/(auth)/auth.config";
-import { NextRequest, NextResponse } from "next/server";
 
-// export default NextAuth(authConfig).auth;
-
-export default function middleware(req: any) {
-  if (req.nextUrl.pathname === "/api/health-check") {
-    return NextResponse.next();
-  }
-  return NextAuth(authConfig).auth(req);
-}
-
+export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: ["/", "/:id", "/api/:path*", "/login", "/register"],
